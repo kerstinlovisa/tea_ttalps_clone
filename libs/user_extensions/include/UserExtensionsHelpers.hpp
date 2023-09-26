e59b60c6e8bf88cca92c51cb9d8c97ebae0babb5
jniedzie <jeremi.niedziela@cern.ch>
2023-09-26 16:21:53 +0200

    Re-organizing ttalps-specific classes/methods

--- libs/user_extensions/include/UserExtensionsHelpers.hpp
+++ libs/user_extensions/include/UserExtensionsHelpers.hpp
@@ -1,7 +1,12 @@
 #ifndef UserExtensionsHelpers_hpp
 #define UserExtensionsHelpers_hpp
 
+#include "TTAlpsEvent.hpp"
 #include "ExtensionsHelpers.hpp"
 #include "PhysicsObject.hpp"
+
+inline std::shared_ptr<TTAlpsEvent> asTTAlpsEvent(const std::shared_ptr<Event> physicsObject) {
+  return std::make_shared<TTAlpsEvent>(physicsObject);
+}
 
 #endif /* UserExtensionsHelpers_hpp */
