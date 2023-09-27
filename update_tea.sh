99636cfae0b877debfac26648d61e47b4e09129c
jniedzie <jeremi.niedziela@cern.ch>
2023-09-27 13:33:06 +0200

    Updating update_tea script

--- update_tea.sh
+++ update_tea.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# save current gitignore 
+cp .gitignore .gitignore_user
+
+# temporarily use tea gitignore
 cp .gitignore_tea .gitignore
+
+# update tea
+git stash
 git pull --rebase upstream main
+git stash pop
+
+# restore user's gitignore
 cp .gitignore_user .gitignore
