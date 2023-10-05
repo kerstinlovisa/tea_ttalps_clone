e64a913c20e500cdaec7c175797c7827670568ad
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-10-05 16:53:26 +0200

    Adding ttZ control region

--- configs/ttalps_analysis/ttalps_histogrammer_list.py
+++ configs/ttalps_analysis/ttalps_histogrammer_list.py
@@ -1,37 +1,40 @@
 max_files = -1
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/signals/tta_mAlp-0p35GeV/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/signals/tta_mAlp-0p35GeV/skimmed_ttbarLike/histograms"
+# skim = "skimmed_ttbarLike"
+skim = "skimmed_ttZLike"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/signals/tta_mAlp-0p35GeV/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/signals/tta_mAlp-0p35GeV/{skim}/histograms"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttWJets/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttWJets/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/{skim}/histograms"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/TTToSemiLeptonic/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/TTToSemiLeptonic/skimmed_ttbarLike/histograms"
+input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttWJets/{skim}"
+output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttWJets/{skim}/histograms"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_antitop/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_antitop/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/TTToSemiLeptonic/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/TTToSemiLeptonic/{skim}/histograms"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_top/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_top/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_antitop/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_antitop/{skim}/histograms"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHToMuMu/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHToMuMu/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_top/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_top/{skim}/histograms"
 
-input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHTobb/skimmed_ttbarLike"
-output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHTobb/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHToMuMu/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHToMuMu/{skim}/histograms"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHTobb/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHTobb/{skim}/histograms"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018B/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018B/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/{skim}/histograms"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018C/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018C/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018B/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018B/{skim}/histograms"
 
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/skimmed_ttbarLike"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/skimmed_ttbarLike/histograms"
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018C/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018C/{skim}/histograms"
+
+# input_directory = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/{skim}"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/{skim}/histograms"
