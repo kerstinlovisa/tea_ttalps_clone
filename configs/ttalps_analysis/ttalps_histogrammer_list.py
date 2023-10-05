30094911f204c48670a1fdc1a64332c9f92be666
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-10-05 08:35:48 +0200

    Updating configs

--- configs/ttalps_analysis/ttalps_histogrammer_list.py
+++ configs/ttalps_analysis/ttalps_histogrammer_list.py
@@ -1,5 +1,8 @@
 max_files = -1
 
+# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/signals/tta_mAlp-0p35GeV/skimmed_ttbarLike"
+# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/signals/tta_mAlp-0p35GeV/skimmed_ttbarLike/histograms"
+
 # input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/skimmed_ttbarLike"
 # output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/skimmed_ttbarLike/histograms"
 
@@ -8,6 +11,18 @@ max_files = -1
 
 # input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/TTToSemiLeptonic/skimmed_ttbarLike"
 # output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/TTToSemiLeptonic/skimmed_ttbarLike/histograms"
+
+# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_antitop/skimmed_ttbarLike"
+# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_antitop/skimmed_ttbarLike/histograms"
+
+# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_top/skimmed_ttbarLike"
+# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_top/skimmed_ttbarLike/histograms"
+
+# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHToMuMu/skimmed_ttbarLike"
+# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHToMuMu/skimmed_ttbarLike/histograms"
+
+input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHTobb/skimmed_ttbarLike"
+output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHTobb/skimmed_ttbarLike/histograms"
 
 # input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/skimmed_ttbarLike"
 # output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/skimmed_ttbarLike/histograms"
@@ -18,5 +33,5 @@ max_files = -1
 # input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018C/skimmed_ttbarLike"
 # output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018C/skimmed_ttbarLike/histograms"
 
-input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/skimmed_ttbarLike"
-output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/skimmed_ttbarLike/histograms"
+# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/skimmed_ttbarLike"
+# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/skimmed_ttbarLike/histograms"
