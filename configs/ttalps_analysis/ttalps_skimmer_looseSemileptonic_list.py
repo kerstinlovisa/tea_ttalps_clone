dbefd36e217249e62db2128f39babf5cae6372eb
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-10-12 08:19:02 +0200

    Cleaning up one of the configs.

--- configs/ttalps_analysis/ttalps_skimmer_looseSemileptonic_list.py
+++ configs/ttalps_analysis/ttalps_skimmer_looseSemileptonic_list.py
@@ -1,20 +1,3 @@
-
-# option 1: list input files and one output directory
-# output files will have the same name as inputs, but be stored the output_dir
-# input_file_list = (
-#   "../samples/signals/nanoAOD_example.root",
-#   "../samples/signals/nanoAOD_example.root"
-# )
-# output_dir = "../samples/signals/skim"
-
-
-# option 2: list input files and output files
-# input_output_file_list = (
-#   ("/nfs/dust/cms/user/jniedzie/ttalps_cms/signals/tta_mAlp-0p35GeV/tta_mAlp-0p35GeV_nEvents-100000.root", 
-#    "/nfs/dust/cms/user/jniedzie/ttalps_cms/signals/tta_mAlp-0p35GeV/skimmed_looseSemileptonic/tta_mAlp-0p35GeV_nEvents-100000.root"),
-# )
-
-# option 3: give DAS dataset name and output path: will run for all files in dataset (or up to max_files)
 max_files = -1
 
 # dataset = "/ttZJets_TuneCP5_13TeV_madgraphMLM_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
@@ -40,7 +23,7 @@ max_files = -1
 
 dataset = "/DYJetsToMuMu_M-50_massWgtFix_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
 output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/DYJetsToMuMu_M-50/skimmed_looseSemileptonic/"
-file_name = "0117C5A4-D21A-7842-BE55-D7B72A7E81C7.root"
+# file_name = "0117C5A4-D21A-7842-BE55-D7B72A7E81C7.root"
 
 # dataset = "/SingleMuon/Run2018A-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"
 # output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/skimmed_looseSemileptonic/"
@@ -54,23 +37,3 @@ file_name = "0117C5A4-D21A-7842-BE55-D7B72A7E81C7.root"
 
 # dataset = "/SingleMuon/Run2018D-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"
 # output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/skimmed_looseSemileptonic/"
-
-# option 4: give DAS dataset name, output path and specify a particular file name you want to run on
-# max_files = -1
-
-# dataset = "/ttZJets_TuneCP5_13TeV_madgraphMLM_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/skimmed_looseSemileptonic/"
-# file_name = "AC05C14C-B6AB-4A40-B647-D1F33544B0C0.root"
-# file_name = "B3D1B575-8F73-F144-A493-199489FB2692.root"
-
-# dataset = "/ttWJets_TuneCP5_13TeV_madgraphMLM_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/skimmed_looseSemileptonic/"
-# file_name = "388501EC-9D5A-A742-B0FE-87D3174835A8.root"
-
-# dataset = "/SingleMuon/Run2018B-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018B/skimmed_looseSemileptonic/"
-# file_name = "E7316692-89C9-284D-9665-F6B18DED9419.root"
-
-# option 5: only specify local input and output directories
-# input_directory = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/skimmed_looseSemileptonic"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/skimmed_ttbarLike/"
