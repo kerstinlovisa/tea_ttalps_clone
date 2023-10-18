9b1c1891e46cfdffdd88ee4c0bf453ba457cffda
jniedzie <jeremi.niedziela@cern.ch>
2023-10-18 11:12:41 +0200

    Improving init script.

--- init.sh
+++ init.sh
@@ -5,3 +5,8 @@ git push origin main
 
 cp .gitignore_user .gitignore
 cp .github/README_user.md .github/README.md
+
+git add .gitignore
+git add .github/README.md
+git commit -m "Initial commit"
+git push
