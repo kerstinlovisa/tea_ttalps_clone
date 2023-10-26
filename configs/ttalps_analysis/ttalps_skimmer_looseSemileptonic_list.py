e0f5f9f02202818e070432b7200ebf33a149c48a
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-10-26 16:35:28 +0200

    Making sure to always apply basic event selections in skimmer. Adding tighter muon selections and a new semimuonic tt CR.

--- configs/ttalps_analysis/ttalps_skimmer_looseSemileptonic_list.py
+++ configs/ttalps_analysis/ttalps_skimmer_looseSemileptonic_list.py
@@ -1,39 +1,92 @@
 max_files = -1
 
+# skim = "skimmed_looseSemileptonic"
+skim = "skimmed_looseSemimuonic_tightMuon"
+
+# Backgrounds
 # dataset = "/ttZJets_TuneCP5_13TeV_madgraphMLM_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/{skim}/"
 
 # dataset = "/ttWJets_TuneCP5_13TeV_madgraphMLM_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttWJets/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttWJets/{skim}/"
 
 # dataset = "/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/TTToSemiLeptonic/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/TTToSemiLeptonic/{skim}/"
 
 # dataset = "/ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5CR1_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_antitop/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_antitop/{skim}/"
 
 # dataset = "/ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5CR1_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_top/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ST_tW_top/{skim}/"
 
 # dataset = "/ttHToMuMu_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHToMuMu/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHToMuMu/{skim}/"
 
 # dataset = "/ttHTobb_ttToSemiLep_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHTobb/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttHTobb/{skim}/"
 
-dataset = "/DYJetsToMuMu_M-50_massWgtFix_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
-output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/DYJetsToMuMu_M-50/skimmed_looseSemileptonic/"
+# dataset = "/DYJetsToMuMu_M-50_massWgtFix_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/DYJetsToMuMu_M-50/{skim}/"
 # file_name = "0117C5A4-D21A-7842-BE55-D7B72A7E81C7.root"
 
+# QCD
+
+# dataset = "/QCD_Pt_15to30_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_15to30/{skim}/"
+
+# dataset = "/QCD_Pt_30to50_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_30to50/{skim}/"
+
+# dataset = "/QCD_Pt_50to80_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_50to80/{skim}/"
+
+# dataset = "/QCD_Pt_80to120_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_80to120/{skim}/"
+
+# dataset = "/QCD_Pt_120to170_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_120to170/{skim}/"
+
+# dataset = "/QCD_Pt_170to300_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_170to300/{skim}/"
+
+# dataset = "/QCD_Pt_300to470_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_300to470/{skim}/"
+
+# dataset = "/QCD_Pt_470to600_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_470to600/{skim}/"
+
+# dataset = "/QCD_Pt_600to800_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_600to800/{skim}/"
+
+# dataset = "/QCD_Pt_800to1000_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_800to1000/{skim}/"
+
+# dataset = "/QCD_Pt_1000to1400_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_1000to1400/{skim}/"
+
+# dataset = "/QCD_Pt_1400to1800_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_1400to1800/{skim}/"
+
+# dataset = "/QCD_Pt_1800to2400_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_1800to2400/{skim}/"
+
+# dataset = "/QCD_Pt_2400to3200_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_2400to3200/{skim}/"
+
+dataset = "/QCD_Pt_3200toInf_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_3200toInf/{skim}/"
+
+
+# Data
 # dataset = "/SingleMuon/Run2018A-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/{skim}/"
 # file_name = "6F8D4F7B-86FE-5241-84C4-D9BB19B19A08.root"
 
 # dataset = "/SingleMuon/Run2018B-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018B/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018B/{skim}/"
 
 # dataset = "/SingleMuon/Run2018C-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018C/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018C/{skim}/"
 
 # dataset = "/SingleMuon/Run2018D-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"
-# output_dir = "/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/skimmed_looseSemileptonic/"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/{skim}/"
