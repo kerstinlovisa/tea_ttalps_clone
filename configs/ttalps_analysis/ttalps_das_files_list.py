97441c4d01362564507e345b6d3fd36ee478ea89
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-10-27 15:44:53 +0200

    Renaming configs to make it more meaningful.

--- configs/ttalps_analysis/ttalps_das_files_list.py
+++ configs/ttalps_analysis/ttalps_das_files_list.py
@@ -3,6 +3,8 @@ max_files = -1
 # skim = "skimmed_looseSemileptonic"
 skim = "skimmed_looseSemimuonic_tightMuon"
 
+# file_name = "6476B810-7ED2-704C-B80D-6F956C63EEBD.root"
+
 # Backgrounds
 # dataset = "/ttZJets_TuneCP5_13TeV_madgraphMLM_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
 # output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/ttZJets/{skim}/"
@@ -27,10 +29,8 @@ skim = "skimmed_looseSemimuonic_tightMuon"
 
 # dataset = "/DYJetsToMuMu_M-50_massWgtFix_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
 # output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/DYJetsToMuMu_M-50/{skim}/"
-# file_name = "0117C5A4-D21A-7842-BE55-D7B72A7E81C7.root"
 
 # QCD
-
 # dataset = "/QCD_Pt_15to30_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
 # output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_15to30/{skim}/"
 
@@ -73,14 +73,13 @@ skim = "skimmed_looseSemimuonic_tightMuon"
 # dataset = "/QCD_Pt_2400to3200_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
 # output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_2400to3200/{skim}/"
 
-dataset = "/QCD_Pt_3200toInf_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
-output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_3200toInf/{skim}/"
+# dataset = "/QCD_Pt_3200toInf_TuneCP5_13TeV_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"
+# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_3200toInf/{skim}/"
 
 
 # Data
 # dataset = "/SingleMuon/Run2018A-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"
 # output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018A/{skim}/"
-# file_name = "6F8D4F7B-86FE-5241-84C4-D9BB19B19A08.root"
 
 # dataset = "/SingleMuon/Run2018B-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"
 # output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018B/{skim}/"
@@ -88,5 +87,5 @@ output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018/QCD_Pt_320
 # dataset = "/SingleMuon/Run2018C-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"
 # output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018C/{skim}/"
 
-# dataset = "/SingleMuon/Run2018D-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"
-# output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/{skim}/"
+dataset = "/SingleMuon/Run2018D-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"
+output_dir = f"/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018/SingleMuon2018D/{skim}/"
