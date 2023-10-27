ef38b783c23cb0ec679c89b259bb7dbdf477c2c7
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-10-27 15:49:28 +0200

    Adding more functionality to count_files script.

--- utils/count_files.py
+++ utils/count_files.py
@@ -2,21 +2,45 @@ import ROOT
 import os
 import sys
 
-sub_path = "skimmed_ttbarLike/histograms/"
+# sub_path = "skimmed_ttbarLike/histograms/"
+sub_path = "skimmed_looseSemimuonic_tightMuon/"
+# sub_path = "skimmed_ttbarSemimuonicCR_tightMuon/"
+
+def count_events_in_files(files):
+    n_events = 0
+    
+    for file in files:
+        f = ROOT.TFile(file)
+        t = f.Get("Events")
+        n_events += t.GetEntries()
+        f.Close()
+    
+    return n_events
+
+def get_total_size_of_files(files):
+    size = 0
+    
+    for file in files:
+        size += os.path.getsize(file)
+    
+    return size
 
 def count_files(base_path):
     paths = [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))]
+    paths = sorted(paths)
 
     for path in paths:
         try:
             files = os.listdir(os.path.join(base_path, path, sub_path))
-            print(f"{path}: {len(files)}")
+            n_events = count_events_in_files([os.path.join(base_path, path, sub_path, file) for file in files])
+            total_size = get_total_size_of_files([os.path.join(base_path, path, sub_path, file) for file in files])
+            print(f"{path}: N files: {len(files)}, N events: {n_events}, size: {total_size/1024/1024/1024:.3f} GB".replace(".", ","))
         except:
             pass
     
 
 if __name__ == "__main__":
-    count_files("backgrounds2018")
-    count_files("collision_data2018")
-    count_files("signals")
+    count_files("/nfs/dust/cms/user/jniedzie/ttalps_cms/backgrounds2018")
+    count_files("/nfs/dust/cms/user/jniedzie/ttalps_cms/collision_data2018")
+    count_files("/nfs/dust/cms/user/jniedzie/ttalps_cms/signals")
     
