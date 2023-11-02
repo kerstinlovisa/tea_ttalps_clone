a11072917cc95fef3097e75f8a99da99f53446e4
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-11-02 15:45:44 +0100

    Adding muon trigger SFs to ttalps histograms.

--- apps/ttalps_analysis/ttalps_histogrammer.cpp
+++ apps/ttalps_analysis/ttalps_histogrammer.cpp
@@ -10,12 +10,12 @@
 using namespace std;
 
 void CheckArgs(int argc, char **argv) {
-  if (argc != 2 && argc != 4 && argc != 5) {
+  if (argc != 2 && argc != 4 && argc != 6) {
     fatal() << "Usage: " << argv[0] << " config_path"<<endl;
     fatal() << "or"<<endl;
     fatal() << argv[0] << " config_path input_path output_path"<<endl;
     fatal() << "or"<<endl;
-    fatal() << argv[0] << " config_path input_path output_path apply_muon_scale_factors"<<endl;
+    fatal() << argv[0] << " config_path input_path output_path apply_muon_scale_factors apply_muon_trigger_scale_factors"<<endl;
     exit(1);
   }
 }
@@ -25,15 +25,20 @@ int main(int argc, char **argv) {
   ConfigManager::Initialize(argv[1]);
   auto &config = ConfigManager::GetInstance();
 
-  if(argc == 4 || argc == 5){
+  if(argc == 4 || argc == 6){
     config.SetInputPath(argv[2]);
     config.SetOutputPath(argv[3]);
   }
-  if(argc == 5){
+  if(argc == 6){
     bool applyMounScaleFactors = atoi(argv[4]);
     if (applyMounScaleFactors) info() << "Muon Scale Factors will be applied" << endl;
     else info() << "Muon Scale Factors were explicitely turned off" << endl;
+
+    bool applyMounTriggerScaleFactors = atoi(argv[5]);
+    if (applyMounTriggerScaleFactors) info() << "Muon Trigger Scale Factors will be applied" << endl;
+    else info() << "Muon Trigger Scale Factors were explicitely turned off" << endl;
     config.SetApplyMuonScaleFactors(applyMounScaleFactors);
+    config.SetApplyMuonTriggerScaleFactors(applyMounTriggerScaleFactors);
   }
 
   auto eventReader = make_shared<EventReader>();
