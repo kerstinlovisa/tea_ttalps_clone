6f6131283202eb4edb690a1df6f8d5bb94d90c02
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-11-03 08:56:03 +0100

    Moving cross sections to a separate file. Adding mu-enriched histograms.

--- configs/ttalps_analysis/ttalps_histogrammer_list.py
+++ configs/ttalps_analysis/ttalps_histogrammer_list.py
@@ -6,6 +6,8 @@ skim = "skimmed_ttbarSemimuonicCR_tightMuon"
 
 base_path = "/nfs/dust/cms/user/jniedzie/ttalps_cms"
 
+doScaleFactors = True
+
 # Backgrounds
 # sample_path = f"backgrounds2018/ttZJets"
 # sample_path = f"backgrounds2018/ttWJets"
@@ -39,18 +41,39 @@ base_path = "/nfs/dust/cms/user/jniedzie/ttalps_cms"
 # sample_path = f"backgrounds2018/QCD_Pt_2400to3200"
 # sample_path = f"backgrounds2018/QCD_Pt_3200toInf"
 
+# QCD mu enhanced
+# sample_path = f"backgrounds2018/QCD_Pt_15to20_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_20to30_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_30to50_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_50to80_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_80to120_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_120to170_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_170to300_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_300to470_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_470to600_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_600to800_MuEnriched"
+# sample_path = f"backgrounds2018/QCD_Pt_800to1000_MuEnriched"
+sample_path = f"backgrounds2018/QCD_Pt_1000_MuEnriched"
+
 # Data
 # sample_path = f"collision_data2018/SingleMuon2018A"
 # sample_path = f"collision_data2018/SingleMuon2018B"
 # sample_path = f"collision_data2018/SingleMuon2018C"
-sample_path = f"collision_data2018/SingleMuon2018D"
+# sample_path = f"collision_data2018/SingleMuon2018D"
 
 
 input_directory = f"{base_path}/{sample_path}/{skim}/"
-output_dir = f"{input_directory}/histograms_noSFs/"
 
-# applyMuonScaleFactors = "collision_data" not in sample_path
-# applyMuonTriggerScaleFactors = "collision_data" not in sample_path
+if doScaleFactors:
+  output_dir = f"{input_directory}/histograms/"
+  applyMuonScaleFactors = "collision_data" not in sample_path
+  applyMuonTriggerScaleFactors = "collision_data" not in sample_path
+else:
+  output_dir = f"{input_directory}/histograms_noSFs/"
+  applyMuonScaleFactors = False
+  applyMuonTriggerScaleFactors = False
 
-applyMuonScaleFactors = False
-applyMuonTriggerScaleFactors = False
+
+
+
+
